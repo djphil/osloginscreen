6e439a82fb53136c3d227d6da6a37f2816747f0c
djphil <djphil@users.noreply.github.com>
2018-12-18 21:34:47 +0100

    Remove .svn folders
    Remove .svn folders

--- css/css.php
+++ css/css.php
@@ -1,6 +1,4 @@
-<?php
-    /* Dynamic CSS v0.1 by djphil (CC-BY-NC-SA 4.0) */
-?>
+<?php /* Dynamic CSS v0.1 by djphil (CC-BY-NC-SA 4.0) */ ?>
 <style>
 @CHARSET "UTF-8";
 .informations,
@@ -11,7 +9,6 @@
 .userlist,
 .logo
 {
-    background:rgba(47,79,79, <?php echo $transparency; ?>);
     filter: alpha(opacity = <?php echo ($transparency * 100.0); ?>);
     -moz-opacity: <?php echo $transparency; ?>;
     opacity: <?php echo $transparency; ?>;
